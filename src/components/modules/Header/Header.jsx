00f2cb66b1958f2a538632840c3783401a3f8f79
Vladimir Andreev <vandreevwork94@gmail.com>
2018-07-18 23:57:02 +0300

    first version

--- src/components/modules/Header/Header.jsx
+++ src/components/modules/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import s from './Header.sass';
+import { Link } from 'react-router-dom';
 
 export class Header extends React.PureComponent {
   render() {
-    return <div className={s.headerWrap}>
-      <div className={s.header} />
+    return <div className={s.header}>
+      <Link to="/" className={s.logo} />
     </div>;
   }
 }
