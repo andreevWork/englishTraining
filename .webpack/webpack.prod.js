e0aade339ff3991597a4af5cef17376ef32420f0
Vladimir Andreev <vandreevwork94@gmail.com>
2018-09-09 21:04:23 +0300

    add copy files after build

--- .webpack/webpack.prod.js
+++ .webpack/webpack.prod.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const distDir = path.resolve(__dirname, '..', 'dist');
+const assetsDir = path.resolve(__dirname, '..', 'assets');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -11,5 +13,11 @@ module.exports = merge(common, {
     filename: 'main.js',
     publicPath: '/',
     path: distDir
-  }
+  },
+  
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: assetsDir, to: path.join(distDir, 'assets') }
+    ])
+  ]
 });
